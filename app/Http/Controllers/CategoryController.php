7925166c5c7e8a2fe20e024a91e11f76fefb769f
Sithmi2001 <sithmiupeksha6@gmail.com>
2024-10-20 22:18:18 +0530

    SEU.IS.20.ICT.046 - Implemented Create function in Controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use app\Models\category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+
+    // Create new category
+    public function create(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+        ]);
+
+        Category::create($request->all());
+        return redirect()->route('category.getAll')->with('success', 'Category created successfully');
+    }
+
 }
